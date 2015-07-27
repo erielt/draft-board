b7ba3908a9269a39ebfb77730cc65dd120f826f5
erielt <erielt@users.noreply.github.com>
2015-07-26 21:33:39 -0700

    Create background.js
    Creates the app window.

---
+++ draft-board/background.js
@@ -0,0 +1,10 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    innerBounds: {
+      width: 800,
+      height: 600,
+      minWidth: 800,
+      minHeight: 600,
+    }
+  });
+});
